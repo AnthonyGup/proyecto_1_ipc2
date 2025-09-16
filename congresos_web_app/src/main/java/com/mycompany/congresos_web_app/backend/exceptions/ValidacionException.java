252f0d80b40388028496becae99c8493d11ddbe7
Anthony <anthonygutierrezp12@gmail.com>
2025-09-16 16:27:51 +0100

    creacion de validaciones iniciales (no hice la rama antes)

---
+++ congresos_web_app/src/main/java/com/mycompany/congresos_web_app/backend/exceptions/ValidacionException.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.congresos_web_app.backend.exceptions;
+
+/**
+ *
+ * @author antho
+ */
+public class ValidacionException extends Exception {
+    
+}
